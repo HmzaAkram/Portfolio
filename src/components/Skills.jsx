c8e3bfbf4ec88511533f2c719bacaf33b7af627a
Itxhamza7 <hmzaakram295@gmail.com>
2024-10-17 18:21:38 +0500

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -15,7 +15,9 @@ const Skills = () => {
                 data-aos="flip-left"
                 data-aos-duration="1000"
               >
-               
+                <img src={`/assets/${data.imageSrc}`} alt="" />
+                <h3>{data.title}</h3>
+              </div>
             </>
           ))}
         </div>
