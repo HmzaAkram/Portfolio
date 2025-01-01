062b8c3cedf1568594c63e6931decfbade106721
Itxhamza7 <hmzaakram295@gmail.com>
2025-01-01 11:58:18 +0500

    pdf_cv

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,17 @@
 import React, { useEffect, useRef } from "react";
-import pdf from "../pdf/resume.pdf";
+import pdf from "../pdf/Hamza_Akram_Cv.pdf";
 import hero from "./data/hero.json";
 import Typed from "typed.js";
 
 const Home = () => {
   const typedRef = useRef(null);
+  
   useEffect(() => {
     const options = {
       strings: [
         "Welcome to my profile",
         "My Name is Hamza Akram",
-        "I'm Fronend developer",
-       
+        "I'm a Frontend Developer",
       ],
       typeSpeed: 50,
       backSpeed: 50,
@@ -26,26 +26,27 @@ const Home = () => {
   }, []);
 
   return (
-    <>
-      <div className="container home" id="home">
-        <div className="left" data-aos="fade-up-right" data-aos-duration="1000">
-          <h1 ref={typedRef}></h1>
+    <div className="container home" id="home">
+      <div className="left" data-aos="fade-up-right" data-aos-duration="1000">
+        <h1 ref={typedRef}></h1>
 
-          <a
-            // href={pdf}
-            download="Resume.pdf"
-            className="btn btn-outline-warning my-3"
-          >
-            Download Resume
-          </a>
-        </div>
-        <div className="right">
-          <div className="img" data-aos="fade-up-left" data-aos-duration="1000">
-            <img src={`/assets/${hero.imgSrc}`} alt="hero" />
-          </div>
+        <a
+          href={pdf} // Link directly to the CV PDF
+          download="Hamza_Akram_Cv.pdf"
+          className="btn btn-outline-warning my-3"
+        >
+          Download Resume
+        </a>
+      </div>
+      <div className="right">
+        <div className="img" data-aos="fade-up-left" data-aos-duration="1000">
+          <img 
+            src={`/assets/${hero.imgSrc}`} 
+            alt={hero.altText || "Hero Image"} // Dynamic alt text for accessibility
+          />
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
