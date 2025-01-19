076d0b89b7b7866e2c4ee403e6d49f94156d62f9
Itxhamza7 <hmzaakram295@gmail.com>
2025-01-19 14:19:59 +0500

    Update contact.jsx

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -24,11 +24,11 @@ const Contact = () => {
                             </div>
                             <div className="flex justify-between items-center">
                                 <span className='md:text-base'>City:</span>
-                                <span className='text-LightGray md:text-sm'>Peshawar</span>
+                                <span className='text-LightGray md:text-sm'>Karachi</span>
                             </div>
                             <div className="flex justify-between items-center">
                                 <span className='md:text-base'>Company:</span>
-                                <span className='text-LightGray md:text-sm'>visualsX</span>
+                                <span className='text-LightGray md:text-sm'></span>
                             </div>
                         </div>
                         <div className="card_stylings rounded-xl w-full md:w-1/2 p-5 md:p-6 lg:p-8 flex flex-col gap-y-4">
