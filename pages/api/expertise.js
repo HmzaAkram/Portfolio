4de8f7ea13f3335b0d284a4ea3a4bc9ef2945dcf
Itxhamza7 <hmzaakram295@gmail.com>
2025-01-21 15:45:06 +0500

    WOrking

--- pages/api/expertise.js
+++ pages/api/expertise.js
@@ -7,25 +7,25 @@ const expertise = [
     {
         id: 1,
         title: 'React JS Developer',
-        desc: "SQL Server Management Studio(SSMS)streamlines database management with tools for queries, analysis, backups, tuning, and monitoring, offering an intuitive interface for efficient administration.",
+        desc: "React.js developer skilled in building dynamic, efficient, and responsive web applications. Expertise in JSX, components, hooks, state management, and integrating APIs for seamless user experiences.",
     },
     {
-        id: 1,
+        id: 2,
         title: 'Github',
         desc: "GitHub is crucial for version control and team collaboration. Features like pull requests, issue tracking, and continuous integration streamline workflows, ensuring organized, up-to-date code for development success.",
     },
     {
-        id: 2,
+        id: 3,
         title: 'Designing (UI/UX)',
         desc: 'I am skilled in Figma, Adobe XD, and Illustrator. Hackathons and freelance work have given me practical UI/UX and graphic design experience, fueling my passion for creativity.',
     },
     {
-        id: 3,
+        id: 4,
         title: 'WordPress',
         desc: "WordPress is a versatile CMS powering over 40% of websites. Its flexibility, themes, plugins, and ease of use enable creating functional sites without coding expertise",
     },
     {
-        id: 4,
+        id: 5,
         title: 'SQLServer Managment',
         desc: "SQL Server Management Studio(SSMS)streamlines database management with tools for queries, analysis, backups, tuning, and monitoring, offering an intuitive interface for efficient administration.",
     },
--- pages/api/recommendations.js
+++ pages/api/recommendations.js
@@ -3,31 +3,31 @@ const recommendationCard = [
   {
     id: 0,
     name: 'Mudassir Malik',
-    image: "images/shahmir.jfif",
+    image: "images/Mudassir.jfif",
     designation: 'React JS | NEXT JS | Node | Video Editor',
-    view: "Hey everyone! I wanted to take a moment to recommend Hamza for any React.js opportunities. I had the pleasure of working with him on multiple projects where he showcased a strong understanding of React.js and Next.js concepts and delivered high-quality code. He consistently demonstrated a willingness to learn and grow, and his enthusiasm for tackling new challenges was contagious. Osama is a reliable and dedicated team member who would be a great asset to any React.js team. Highly recommended! 👍! 🌟",
+    view: "Hey everyone! I wanted to take a moment to recommend Hamza for any React.js opportunities. I had the pleasure of working with him on multiple projects where he showcased a strong understanding of React.js and Next.js concepts and delivered high-quality code. He consistently demonstrated a willingness to learn and grow, and his enthusiasm for tackling new challenges was contagious. Hamza is a reliable and dedicated team member who would be a great asset to any React.js team. Highly recommended! 👍! 🌟",
     linkednURL: "https://www.linkedin.com/"
   },
   {
     id: 1,
     name: 'Zaki Haider',
-    image: "images/sikandar.jpeg",
+    image: "images/Zaki.jpeg",
     designation: 'JavaScript | Tailwind | Sass | Bootstrap | Python Developer',
-    view: "I had the privilege of working alongside Hamza on multiple College projects, and I must say, his talent and work ethic truly stood out. Recently, we were both part of the dynamic team, where Hamza's professionalism and attention to detail shone brightly. I wholeheartedly endorse Osama for any opportunity that calls for a highly skilled and dedicated individual.",
+    view: "I had the privilege of working alongside Hamza on multiple College projects, and I must say, his talent and work ethic truly stood out. Recently, we were both part of the dynamic team, where Hamza's professionalism and attention to detail shone brightly. I wholeheartedly endorse Hamza for any opportunity that calls for a highly skilled and dedicated individual.",
     linkednURL: "https://www.linkedin.com/"
   },
   {
     id: 2,
     name: 'Ehtisham Aktar',
-    image: "images/nasir.jpg",
+    image: "images/Ehtisham.jpg",
     designation: 'Next Js | JavaScript | Tailwind | Sass | Bootstrap',
-    view: "I wholeheartedly recommend Osama as a talented React frontend developer with an incredible flair for UI/UX design. His proficiency in Node.js further enhances his capabilities, allowing him to build robust and scalable applications. Osama's attention to detail, problem-solving skills, and dedication to delivering exceptional results make him a valuable addition to any development team.",
+    view: "I wholeheartedly recommend Hamza as a talented React frontend developer with an incredible flair for UI/UX design. His proficiency in Node.js further enhances his capabilities, allowing him to build robust and scalable applications. Hamza's attention to detail, problem-solving skills, and dedication to delivering exceptional results make him a valuable addition to any development team.",
     linkednURL: "https://www.linkedin.com/"
   },
   {
     id: 3,
     name: 'Muhammad Afan',
-    image: "images/muhammad.jpeg",
+    image: "images/Afan.jpeg",
     designation: 'MERN Stack Developer at Productbox',
     view: "I highly recommend Hamza Akram for web frontend development positions. Their expertise in ReactJS and Next.js, combined with their professionalism and dedication, make them an invaluable asset to any team.",
     linkednURL: "https://www.linkedin.com/in/"
