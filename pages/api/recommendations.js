a34e98ca2e32fba7e2192b0dc81de202277a5df1
Itxhamza7 <hmzaakram295@gmail.com>
2025-01-21 15:24:12 +0500

    Update recommendations.js

--- pages/api/recommendations.js
+++ pages/api/recommendations.js
@@ -13,7 +13,7 @@ const recommendationCard = [
     name: 'Zaki Haider',
     image: "images/sikandar.jpeg",
     designation: 'JavaScript | Tailwind | Sass | Bootstrap | Python Developer',
-    view: "I had the privilege of working alongside Osama on multiple university projects, and I must say, his talent and work ethic truly stood out. Recently, we were both part of the dynamic team at JMM Technologies, where Osama's professionalism and attention to detail shone brightly. I wholeheartedly endorse Osama for any opportunity that calls for a highly skilled and dedicated individual.",
+    view: "I had the privilege of working alongside Hamza on multiple College projects, and I must say, his talent and work ethic truly stood out. Recently, we were both part of the dynamic team, where Hamza's professionalism and attention to detail shone brightly. I wholeheartedly endorse Osama for any opportunity that calls for a highly skilled and dedicated individual.",
     linkednURL: "https://www.linkedin.com/"
   },
   {
