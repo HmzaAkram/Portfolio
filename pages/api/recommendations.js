3b6b62e667533d630b9cc28249590d9d496d68c8
Itxhamza7 <hmzaakram295@gmail.com>
2025-01-21 15:23:17 +0500

    Update recommendations.js

--- pages/api/recommendations.js
+++ pages/api/recommendations.js
@@ -6,15 +6,15 @@ const recommendationCard = [
     image: "images/shahmir.jfif",
     designation: 'React JS | NEXT JS | Node | Video Editor',
     view: "Hey everyone! I wanted to take a moment to recommend Hamza for any React.js opportunities. I had the pleasure of working with him on multiple projects where he showcased a strong understanding of React.js and Next.js concepts and delivered high-quality code. He consistently demonstrated a willingness to learn and grow, and his enthusiasm for tackling new challenges was contagious. Osama is a reliable and dedicated team member who would be a great asset to any React.js team. Highly recommended! 👍! 🌟",
-    linkednURL: "https://www.linkedin.com/in/"
+    linkednURL: "https://www.linkedin.com/"
   },
   {
     id: 1,
-    name: 'Sikandar Hayat',
+    name: 'Zaki Haider',
     image: "images/sikandar.jpeg",
-    designation: 'Data Scientist | Machine Learning Engineer | Python Developer',
+    designation: 'JavaScript | Tailwind | Sass | Bootstrap | Python Developer',
     view: "I had the privilege of working alongside Osama on multiple university projects, and I must say, his talent and work ethic truly stood out. Recently, we were both part of the dynamic team at JMM Technologies, where Osama's professionalism and attention to detail shone brightly. I wholeheartedly endorse Osama for any opportunity that calls for a highly skilled and dedicated individual.",
-    linkednURL: "https://www.linkedin.com/in/sikandar-hayat-381407179/"
+    linkednURL: "https://www.linkedin.com/"
   },
   {
     id: 2,
